cbd7528f4ea54d868d55a2492f1ea8628dd266cc
21A91A0484 <98581531+21A91A0484@users.noreply.github.com>
2023-09-27 15:10:14 +0530

    Add files via upload

---
+++ react_project/src/projects/clicked-modal/clickedModalData.js
@@ -0,0 +1,22 @@
+const clickedModalData = [
+    {
+        id: 1,
+        img: "https://img.icons8.com/color/2x/angularjs.png",
+        title: 'JS',
+        desc: 'JavaScript is a scripting or programming language that allows you to implement complex features on web pages'
+    },
+    {
+        id: 2,
+        img: "https://img.icons8.com/color/2x/react-native.png",
+        title: 'React JS',
+        desc: 'React is a declarative, efficient, and flexible JavaScript library for building user interfaces based on UI components.'
+    },
+    {
+        id: 3,
+        img: "https://img.icons8.com/color/2x/vue-js.png",
+        title: 'HTML',
+        desc: 'To understand "HTML" from front to back, let look at each word that makes up the abbreviation: Hypertext: text (often with embeds such as images, too) that is organized in order to connect related items Markup: a style guide for typesetting anything to be printed in hardcopy or soft copy format'
+    },
+];
+
+export default clickedModalData;
